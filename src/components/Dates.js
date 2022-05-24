f1881756d8823906c8e88e80435375e31d12c19d
WKDStevie <50949225+StefanBartl@users.noreply.github.com>
2022-05-24 07:39:50 +0200

    update

--- src/components/Dates.js
+++ src/components/Dates.js
@@ -2,32 +2,47 @@ import React from 'react';
 
 export default function Dates(){
 
+    const [ dates, setDates ] = React.useState([]);
+
+    function datesEvent(event){
+            setDates(prevState => 
+                [
+                    prevState[0]= event.target.value
+                ]
+            )
+            console.log(dates)
+    };
+
+
     return (
         <div className='dates'>
 
-            <div className='namesDiv'>
-                <p htmlFor='firstName'>First name</p>
-                <input type='text' name='firstName'></input>
-                <p htmlFor='firstName'>Last name</p>
-                <input type='text' name='lastName'></input>
-            </div>
+            <div className='datesInputs'>
+                <div className='namesDiv'>
+                    <p htmlFor='firstName'>First name</p>
+                    <input type='text' name='firstName' placeholder='John' onChange={datesEvent} value={dates[0]}></input>
+                    <p htmlFor='firstName'>Last name</p>
+                    <input type='text' name='lastName' placeholder='Doe'></input>
+                </div>
 
-            <div className='adressDiv'>
-                <p htmlFor='adress'>Adress</p>
-                <input type='text' name='adress'></input>
-                <p htmlFor='postCode'>Post code</p>
-                <input type='number' name='postCode'></input>
-                <p htmlFor='city'>City</p>
-                <input type='text' name='city'></input>
-            </div>
+                <div className='adressDiv'>
+                    <p htmlFor='adress'>Adress</p>
+                    <input type='text' name='adress' placeholder='Empty Street 10'></input>
+                    <p htmlFor='postCode'>Post code</p>
+                    <input type='number' name='postCode' placeholder='10909'></input>
+                    <p htmlFor='city'>City</p>
+                    <input type='text' name='city' placeholder='Gotham City'></input>
+                </div>
 
-            <div className='contactDiv'>
-                <p htmlFor='phone'>Phone</p>
-                <input type='number' name='phone'></input>
-                <p htmlFor='email'>Email</p>
-                <input type='email' name='email'></input>
-            </div>  
-            <input type='submit' name='submit'></input>
+                <div className='contactDiv'>
+                    <p htmlFor='phone'>Phone</p>
+                    <input type='number' name='phone' placeholder='43 664 7878111'></input>
+                    <p htmlFor='email'>Email</p>
+                    <input type='email' name='email' placeholder='joe.doe@mail.com'></input>
+                </div>  
+                <input type='submit' name='submit'></input>
+            </div>
+            <h1>{dates.firstName}</h1>
         </div>
     )
 
