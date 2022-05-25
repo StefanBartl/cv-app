33a86c514997a9cdad8003793737ddede13eba66
WKDStevie <50949225+StefanBartl@users.noreply.github.com>
2022-05-25 23:25:59 +0200

    Finished dates fom incl. lock/edit function and storing the data in app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
+import React from 'react';
 import Dates from './components/Dates';
 import './App.css';
 
 export default function App() {
+
+  const [ submittedData, setSubmittedData] = React.useState({
+    firstName: "", 
+    lastName: "", 
+    adress: "", 
+    postCode: 0, 
+    city: "", 
+    phone: 0,
+    email: ""  
+  })
+  // console.log(submittedData);
+
+  function collectData(data){
+
+    // ? Get submitted data from 'Dates'
+    if(data.firstName){
+      setSubmittedData(prevData => {
+        return {
+          ...prevData,
+          firstName: data.firstName, 
+          lastName: data.lastName, 
+          adress: data.adress, 
+          postCode: data.postCode, 
+          city: data.city, 
+          phone: data.phone,
+          email: data.email  
+        }
+      })
+      // console.log("Data from Dates receivedl");
+    };
+
+};
+
+
   return (
     <div className="App">
       <h1 className='title'>WKD Steve's CV-APP</h1>
-      <Dates />
+      <Dates sendData={collectData}/>
     </div>
   );
 }
--- src/components/Dates.js
+++ src/components/Dates.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Dates(){
+export default function Dates(props){
 
     const [ dates, setDates ] = React.useState(
         {
@@ -10,7 +10,7 @@ export default function Dates(){
             postCode: 0, 
             city: "", 
             phone: 0,
-            email: 0  
+            email: ""  
       }
     );
 
@@ -25,12 +25,27 @@ export default function Dates(){
             });
     };
 
-
     function handleSubmit(event){
+        // ? Send actual values from the input fields to App component
         event.preventDefault();
-        // ! set values?
-    }
+        props.sendData(dates)
+    };
 
+    let editState = false;
+    function edit(event){
+
+        // Toggle edit state
+        editState === false ? editState = true : editState = false;
+        // Get all elements
+       let inputsArray =  document.querySelectorAll('.inputFields');
+       inputsArray.forEach(element => {
+           // Enable editing (via css-rule pointer-events: all) or lock it depending on the editState boolean 
+        editState === true ? element.classList.add("editInputFields") : element.classList.remove("editInputFields");
+       });
+       
+
+    // console.log(editState)
+    };
 
     return (
         <div className='dates'>
@@ -39,32 +54,32 @@ export default function Dates(){
 
                 <div className='namesDiv'>
                     <p htmlFor='firstName'>First name</p>
-                    <input type='text' name='firstName' placeholder='John' onChange={datesEvent}></input>
+                    <input type='text' name='firstName' className='inputFields' placeholder='John' value={dates.firstName} onChange={datesEvent}  />
                     <p htmlFor='firstName'>Last name</p>
-                    <input type='text' name='lastName' placeholder='Doe'></input>
+                    <input type='text' name='lastName' className='inputFields' placeholder='Doe' value={dates.lastName} onChange={datesEvent} />
                 </div>
 
                 <div className='adressDiv'>
                     <p htmlFor='adress'>Adress</p>
-                    <input type='text' name='adress' placeholder='Empty Street 10'></input>
+                    <input type='text' name='adress' className='inputFields' placeholder='Empty Street 10' value={dates.adress} onChange={datesEvent} />
                     <p htmlFor='postCode'>Post code</p>
-                    <input type='number' name='postCode' placeholder='10909'></input>
+                    <input type='number' name='postCode' className='inputFields'  placeholder='10909' value={dates.postCode} onChange={datesEvent} />
                     <p htmlFor='city'>City</p>
-                    <input type='text' name='city' placeholder='Gotham City'></input>
+                    <input type='text' name='city' className='inputFields' placeholder='Gotham City' value={dates.city} onChange={datesEvent} />
                 </div>
 
                 <div className='contactDiv'>
                     <p htmlFor='phone'>Phone</p>
-                    <input type='number' name='phone' placeholder='43 664 7878111'></input>
+                    <input type='number' name='phone' className='inputFields' placeholder='43 664 7878111' value={dates.phone} onChange={datesEvent}/>
                     <p htmlFor='email'>Email</p>
-                    <input type='email' name='email' placeholder='joe.doe@mail.com'></input>
+                    <input type='email' name='email' className='inputFields' placeholder='joe.doe@mail.com' value={dates.email} onChange={datesEvent} />
                 </div>  
 
-                <input type='submit' name='submit' value="OK"></input>
+                <input type='button' name='edit' value="edit" onClick={edit} />
+                <input type='submit' name='submit' value="ok" onClick={handleSubmit} />
             
             </div>
         </div>
-    )
-
+        )
 };
 
